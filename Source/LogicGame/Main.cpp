20f4d2207b18d0a901db829ee4d57dee84feafd0
Nikolay Despotov <85335497+NVDespotov20@users.noreply.github.com>
2022-04-30 14:17:33 +0300

    Make button
    Co-Authored-By: Yoan Todorov <85336778+YYTodorov20@users.noreply.github.com>
    Co-Authored-By: Aleksander Kostov <85336796+AIKostov20@users.noreply.github.com>

--- Source/LogicGame/Main.cpp
+++ Source/LogicGame/Main.cpp
@@ -6,26 +6,42 @@ using namespace std;
 using namespace sf; 
 
 
-int widthX = sf::VideoMode::getDesktopMode().width;
-int heightY = sf::VideoMode::getDesktopMode().height;
+int widthX = VideoMode::getDesktopMode().width;
+int heightY = VideoMode::getDesktopMode().height;
 
 
 int main()
 {
-    RenderWindow window(sf::VideoMode(0.f,0.f),"Booleo", Style::Fullscreen);
+    RenderWindow window(VideoMode(0.f,0.f),"Booleo", Style::Fullscreen);
     RectangleShape shape(Vector2f(widthX,heightY));
-    shape.setFillColor(sf::Color::White);
-
+    RectangleShape button(Vector2f(widthX / 11, heightY / 5));
+    button.setFillColor(Color::Cyan);
+    Font fon;
+    fon.loadFromFile("Fonts/arial.ttf");
+    Text t;
+    t.setFont(fon);
+    t.setCharacterSize(50);
+    t.setFillColor(Color::Black);
+    t.setPosition(Vector2f(widthX / 2, heightY / 2));
+    t.setOrigin(Vector2f(100.f,25.f));
+    t.setString("FlashBANG");
+    shape.setFillColor(Color::White);
     while (window.isOpen())
     {
        
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
         {
             window.close();
         }
+        
 
         window.clear();
         window.draw(shape);
+        window.draw(button);
+        if (Mouse::isButtonPressed(Mouse::Left) && button.getLocalBounds().contains(Vector2f(Mouse::getPosition())))
+        {
+            window.draw(t);
+        }
         window.display();
     }
 
