981ce4892f99f903083e2f5edd10f9437325673f
AIKostov20 <aikostov20@codingburgas.bg>
2022-04-30 17:38:54 +0300

    Crate the drag and drop mechanic

--- Source/LogicGame/Main.cpp
+++ Source/LogicGame/Main.cpp
@@ -10,10 +10,16 @@ int widthX = VideoMode::getDesktopMode().width;
 int heightY = VideoMode::getDesktopMode().height;
 
 
+
 int main()
 {
-    RenderWindow window(VideoMode(0.f,0.f),"Booleo", Style::Fullscreen);
+    RenderWindow window(VideoMode(widthX, heightY),"Booleo", Style::Fullscreen);
+
+    
     RectangleShape shape(Vector2f(widthX,heightY));
+
+    shape.setFillColor(Color::White);
+
     RectangleShape button(Vector2f(widthX / 11, heightY / 5));
     button.setFillColor(Color::Cyan);
     Font fon;
@@ -23,9 +29,16 @@ int main()
     t.setCharacterSize(50);
     t.setFillColor(Color::Black);
     t.setPosition(Vector2f(widthX / 2, heightY / 2));
-    t.setOrigin(Vector2f(100.f,25.f));
+    t.setOrigin(Vector2f(100.f, 25.f));
     t.setString("FlashBANG");
-    shape.setFillColor(Color::White);
+
+    RectangleShape cardHand1(Vector2f(120.f, 150.f));
+    cardHand1.setFillColor(Color::Cyan);
+    cardHand1.setPosition(60.f, heightY/2);
+    cardHand1.setOrigin(Vector2f(60.f,75.f));
+   
+
+
     while (window.isOpen())
     {
        
@@ -37,11 +50,17 @@ int main()
 
         window.clear();
         window.draw(shape);
-        window.draw(button);
-        if (Mouse::isButtonPressed(Mouse::Left) && button.getLocalBounds().contains(Vector2f(Mouse::getPosition())))
+       
+
+        if (Mouse::isButtonPressed(Mouse::Left) && cardHand1.getGlobalBounds().contains(Vector2f(Mouse::getPosition())))
         {
-            window.draw(t);
+            
+                    sf::Vector2i mousePos =  sf::Mouse::getPosition(window);
+                    cardHand1.setPosition((float)mousePos.x, (float)mousePos.y);
         }
+        
+        window.draw(cardHand1);
+        window.draw(t);
         window.display();
     }
 
